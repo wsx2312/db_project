db23adb991a3cee1f11e2756060a4f1900762939
조신영(mac) <>
2022-09-26 22:15:02 +0900

    refactor: task5 with clause

--- project1/submitted/task5.sql
+++ project1/submitted/task5.sql
@@ -1,25 +1,25 @@
 select gt."DAY_OF_WEEK", format('%s ~ %s', to_char(tt."START_TIME", 'FMHH24:MI'), to_char(tt."END_TIME", 'FMHH24:MI')) time
 from
 (
+	with student_timetable as (
+		select ctt."DAY_OF_WEEK", ctt."NO", cr."STUDENT_ID" from course_registration cr
+			join course c on cr."COURSE_ID" = c."COURSE_ID"
+			right join course_to_time ctt on c."COURSE_ID" = ctt."COURSE_ID"
+		where c."YEAR" = 2018 and c."SEMESTER" = 2
+	)
 	select dow."DAY_OF_WEEK", "NO" from day_of_week dow 
 	cross join timetable t 
 	where dow."DAY_OF_WEEK" != 'SAT' 
 	and dow."DAY_OF_WEEK" != 'SUN'
 	except 
 	(
-		select ctt."DAY_OF_WEEK", ctt."NO" from course_registration cr
-		join course c on cr."COURSE_ID" = c."COURSE_ID"
-		right join course_to_time ctt on c."COURSE_ID" = ctt."COURSE_ID"
-		where "STUDENT_ID" = '2018111111' and 
-		c."YEAR" = 2018 and c."SEMESTER" = 2
+		select "DAY_OF_WEEK", "NO" from student_timetable
+		where "STUDENT_ID" = '2018111111'
 		
 		union
 		
-		select ctt."DAY_OF_WEEK", ctt."NO" from course_registration cr
-		join course c on cr."COURSE_ID" = c."COURSE_ID"
-		right join course_to_time ctt on c."COURSE_ID" = ctt."COURSE_ID"
-		where "STUDENT_ID" = '2017222222' and 
-		c."YEAR" = 2018 and c."SEMESTER" = 2
+		select "DAY_OF_WEEK", "NO" from student_timetable
+		where "STUDENT_ID" = '2017222222'
 	)
 ) gt
 join timetable tt
