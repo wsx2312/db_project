30b4529fe3e69f9b945937ae4d432890af7aff72
조신영(mac) <>
2022-09-27 17:39:36 +0900

    fix: task6 avg grade calculation

--- project1/submitted/task6.sql
+++ project1/submitted/task6.sql
@@ -1,5 +1,33 @@
-with graduate_student as (
-	select s."STUDENT_ID", sum(g."GRADE" * c."CREDIT") / sum(c."CREDIT") as "AVG_GRADE" from students s
+-- with graduate_student as (
+-- 	select s."STUDENT_ID", sum(g."GRADE" * c."CREDIT") / sum(c."CREDIT") as "AVG_GRADE" from students s
+-- 	join grade g
+-- 		on g."STUDENT_ID" = s."STUDENT_ID"
+-- 	join course c
+-- 		on c."COURSE_ID" = g."COURSE_ID"
+-- 	where (s."STUDENT_ID", c."COURSE_ID_PREFIX", c."COURSE_ID_NO", concat(c."YEAR", c."SEMESTER"))
+-- 	in (
+-- 		select s."STUDENT_ID", c."COURSE_ID_PREFIX", c."COURSE_ID_NO", max(concat(c."YEAR", c."SEMESTER")) "YEAR_SEMESTER" from students s
+-- 			join grade g
+-- 				on g."STUDENT_ID" = s."STUDENT_ID"
+-- 			join course c
+-- 				on c."COURSE_ID" = g."COURSE_ID"
+-- 		where s."ADMISSION_YEAR" = 2015
+-- 		group by (s."STUDENT_ID", c."COURSE_ID_PREFIX", c."COURSE_ID_NO")
+-- 	)
+-- 	and g."GRADE" != 0
+-- 	group by s."STUDENT_ID"
+-- 	having sum(c."CREDIT") >= 40
+-- )
+-- select concat(left(s."NAME", 1), repeat('*', length(s."NAME") - 1)), s."STUDENT_ID", c."MAJOR_NAME", c."COLLEGE_NAME" from graduate_student gs
+-- join students s
+-- 	on gs."STUDENT_ID" = s."STUDENT_ID"
+-- join college c 
+-- 	on s."MAJOR_ID" = c."MAJOR_ID"
+-- where gs."AVG_GRADE" = (select max("AVG_GRADE") from graduate_student) 
+-- 	or gs."AVG_GRADE" = (select min("AVG_GRADE") from graduate_student) 
+
+with grade4_with_no_retake as  (
+  select s."STUDENT_ID", g."GRADE", c."CREDIT", s."NAME" from students s 
 	join grade g
 		on g."STUDENT_ID" = s."STUDENT_ID"
 	join course c
@@ -11,17 +39,38 @@ with graduate_student as (
 				on g."STUDENT_ID" = s."STUDENT_ID"
 			join course c
 				on c."COURSE_ID" = g."COURSE_ID"
-		where s."ADMISSION_YEAR" = 2015
+		where s."GRADE" = 4
 		group by (s."STUDENT_ID", c."COURSE_ID_PREFIX", c."COURSE_ID_NO")
 	)
-	and g."GRADE" != 0
-	group by s."STUDENT_ID"
-	having sum(c."CREDIT") >= 40
 )
-select concat(left(s."NAME", 1), repeat('*', length(s."NAME") - 1)), s."STUDENT_ID", c."MAJOR_NAME", c."COLLEGE_NAME" from graduate_student gs
+select concat(left(s."NAME", 1), repeat('*', length(s."NAME") - 1)), s."STUDENT_ID", c."MAJOR_NAME", c."COLLEGE_NAME" from (
+	select "STUDENT_ID" from grade4_with_no_retake gs
+		where "GRADE" != 0
+	group by "STUDENT_ID"
+	having sum("CREDIT") >= 40
+) gs
+join
+(
+	select "STUDENT_ID", sum("GRADE" * "CREDIT") / sum("CREDIT") as "AVG_GRADE" 
+		from grade4_with_no_retake gs
+	group by "STUDENT_ID"
+) ag
+	on gs."STUDENT_ID" = ag."STUDENT_ID"
 join students s
 	on gs."STUDENT_ID" = s."STUDENT_ID"
 join college c 
 	on s."MAJOR_ID" = c."MAJOR_ID"
-where gs."AVG_GRADE" = (select max("AVG_GRADE") from graduate_student) 
-	or gs."AVG_GRADE" = (select min("AVG_GRADE") from graduate_student) 
+where ag."AVG_GRADE" = (select max("AVG_GRADE") from 
+		(
+			select "STUDENT_ID", sum("GRADE" * "CREDIT") / sum("CREDIT") as "AVG_GRADE" 
+				from grade4_with_no_retake gs
+			group by "STUDENT_ID"
+		) t
+	) 
+	or ag."AVG_GRADE" = (select min("AVG_GRADE") from 
+		(
+			select "STUDENT_ID", sum("GRADE" * "CREDIT") / sum("CREDIT") as "AVG_GRADE" 
+				from grade4_with_no_retake gs
+			group by "STUDENT_ID"
+		) t
+	) 
