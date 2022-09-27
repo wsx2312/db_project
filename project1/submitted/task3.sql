b72df0997414837496030851e881c3ddcf1505a2
Shin Hyungsik <wsx2312@naver.com>
2022-09-27 22:58:24 +0900

    Submit for Problem 1, 2 and 3.

--- project1/submitted/task3.sql
+++ project1/submitted/task3.sql
@@ -1,16 +1,10 @@
-select f."NAME", p."CLASS_COUNT"
-from (
-	select c."PROF_ID", count(*) as "CLASS_COUNT" from course c
-	join (
-		select "COURSE_ID", count(*) as "STUDENT_COUNT" 
-		from "course_registration"
-		group by "COURSE_ID"
-	) cr
-	on c."COURSE_ID" = cr."COURSE_ID"
-	where c."YEAR" between 2013 and 2018
-	and cr."STUDENT_COUNT" > 0.8 * c."MAX_ENROLLEES"
-	group by c."PROF_ID"
-) p
-join faculty f
-on p."PROF_ID" = f."ID"
-order by p."CLASS_COUNT" desc, f."NAME"
+SELECT C."NAME", COUNT(C."ID") 
+FROM faculty as C, course as D 
+WHERE C."ID" = D."PROF_ID" and D."COURSE_ID" in (
+SELECT A."COURSE_ID" 
+FROM course as A, course_registration as B 
+WHERE A."COURSE_ID" = B."COURSE_ID" 
+GROUP BY A."COURSE_ID" 
+HAVING COUNT(B."STUDENT_ID")::decimal / A."MAX_ENROLLEES"::decimal > 0.8) 
+GROUP BY C."ID" 
+ORDER BY COUNT(C."ID") desc, C."NAME" asc;
