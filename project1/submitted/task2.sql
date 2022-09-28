9941721b35da53b72dfae72fee44b0dd11bab2fb
조신영(mac) <>
2022-09-28 23:05:56 +0900

    style: format task 1, 2, 3

--- project1/submitted/task2.sql
+++ project1/submitted/task2.sql
@@ -1,11 +1,24 @@
-SELECT A."COURSE_NAME" 
-FROM course as A, course_registration as B 
-WHERE A."COURSE_ID" = B."COURSE_ID" and B."STUDENT_ID" in (
-SELECT D."STUDENT_ID" 
-FROM course as C, course_registration as D 
-WHERE C."COURSE_ID" = D."COURSE_ID" 
-GROUP BY D."STUDENT_ID" 
-HAVING COUNT(D."STUDENT_ID") > 1) 
-GROUP BY A."COURSE_NAME" 
-ORDER BY COUNT(B."STUDENT_ID") DESC 
+SELECT
+  A."COURSE_NAME"
+FROM
+  course AS A
+  , course_registration AS B
+WHERE
+  A."COURSE_ID" = B."COURSE_ID"
+  AND B."STUDENT_ID" IN (
+    SELECT
+      D."STUDENT_ID"
+    FROM
+      course AS C
+      , course_registration AS D
+    WHERE
+      C."COURSE_ID" = D."COURSE_ID"
+    GROUP BY
+      D."STUDENT_ID"
+    HAVING
+      COUNT(D."STUDENT_ID") > 1)
+GROUP BY
+  A."COURSE_NAME"
+ORDER BY
+  COUNT(B."STUDENT_ID") DESC
 LIMIT 3;
--- project1/submitted/task3.sql
+++ project1/submitted/task3.sql
@@ -1,10 +1,25 @@
-SELECT C."NAME", COUNT(C."ID") 
-FROM faculty as C, course as D 
-WHERE C."ID" = D."PROF_ID" and D."COURSE_ID" in (
-SELECT A."COURSE_ID" 
-FROM course as A, course_registration as B 
-WHERE A."COURSE_ID" = B."COURSE_ID" 
-GROUP BY A."COURSE_ID" 
-HAVING COUNT(B."STUDENT_ID")::decimal / A."MAX_ENROLLEES"::decimal > 0.8) 
-GROUP BY C."ID" 
-ORDER BY COUNT(C."ID") desc, C."NAME" asc;
+SELECT
+  C."NAME"
+  , COUNT(C."ID")
+FROM
+  faculty AS C
+  , course AS D
+WHERE
+  C."ID" = D."PROF_ID"
+  AND D."COURSE_ID" IN (
+    SELECT
+      A."COURSE_ID"
+    FROM
+      course AS A
+      , course_registration AS B
+    WHERE
+      A."COURSE_ID" = B."COURSE_ID"
+    GROUP BY
+      A."COURSE_ID"
+    HAVING
+      COUNT(B."STUDENT_ID")::decimal / A."MAX_ENROLLEES"::decimal > 0.8)
+GROUP BY
+  C."ID"
+ORDER BY
+  COUNT(C."ID") DESC
+  , C."NAME" ASC;
